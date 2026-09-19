641f844f91b7108bc03d42e6b7aa6a7272387f3b
fearvers <alyson.doll33@gmail.com>
2026-09-19 12:14:37 +0200

    Fix config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const CLIENT_ID = "adc9c9fee3cb4aefb0d11977858a4036";
-xport const REDIRECT_URI = "https://ton-url-vercel.vercel.app";
+ export const REDIRECT_URI = "https://spotify-meteo-abc.vercel.app";
